5a74ddd3aac205c22470973a817982287afd9228
lyubogeorgiev <lyubo12georgiev@gmail.com>
2023-12-10 13:11:25 -0500

    lab 1 "Browser History", and lab 2 "Simple Calculator" by using a stack data structures were added from the lesson for Stacks and Queues.

---
+++ Stack_Queue/src/l01BrowserHistory.java
@@ -0,0 +1,36 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class l01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<String> stack = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        while(!input.equals("Home")){
+            if (input.equals("back")){
+                if (!stack.isEmpty()){
+                    String currentUrl = stack.pop();
+
+                    if (stack.isEmpty()){
+                        stack.push(currentUrl);
+                        System.out.println("no previous URLs");
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                } else {
+                    System.out.println("no previous URLs");
+                }
+
+            } else {
+                stack.push(input);
+                System.out.println(stack.peek());
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}
---
+++ Stack_Queue/src/l02SimpleCalculator.java
@@ -0,0 +1,44 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class l02SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] tokens = scanner.nextLine().split("\\s+");
+
+        Deque<String> signs = new ArrayDeque<>();
+        Deque<Integer> numbers = new ArrayDeque<>();
+
+        int result = 0;
+
+        for (int i = tokens.length - 1; i >= 0; i--) {
+            if (tokens[i].equals("-") || tokens[i].equals("+")){
+                signs.push(tokens[i]);
+            } else {
+                int number = Integer.parseInt(tokens[i]);
+                numbers.push(number);
+            }
+        }
+
+        while(!numbers.isEmpty() && !signs.isEmpty()){
+            int firstNum = numbers.pop();
+            int secondNum = numbers.pop();
+
+            String sign = signs.pop();
+
+            if (sign.equals("+")){
+                numbers.push(firstNum + secondNum);
+            } else {
+                numbers.push(firstNum - secondNum);
+            }
+        }
+
+        if (!numbers.isEmpty()){
+
+            System.out.println(numbers.peek());
+        }
+    }
+}
