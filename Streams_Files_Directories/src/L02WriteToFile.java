054ff7cd84d34b9fe2058ecbeb86c97b2f40f3b3
lyubogeorgiev <lyubo12georgiev@gmail.com>
2024-01-17 13:57:16 -0500

    Lab 02 "Write to File" from the lesson for Streams, Files and Directories, a part of Java Advanced course.

--- Streams_Files_Directories/src/L02WriteToFile.java
+++ Streams_Files_Directories/src/L02WriteToFile.java
@@ -1,15 +1,11 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class L02WriteToFile {
     public static void main(String[] args) {
         String inputPath = "C:\\WGU\\Java-Advanced-1\\Streams_Files_Directories\\Resources\\input.txt";
         String outputPath = "C:\\WGU\\Java-Advanced-1\\Streams_Files_Directories\\Resources\\output.txt";
-
-        String textToBeWritten = "Some text here";
 
         String prohibitedSymbols = ".,!?";
 
@@ -26,7 +22,6 @@ public class L02WriteToFile {
             }
 
             fileOutputStream.flush();
-            fileInputStream.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
