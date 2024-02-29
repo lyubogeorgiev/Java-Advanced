61f9007c8afefa1fe3e2f272cf4fe8b6bdd87a22
lyubogeorgiev <lyubo12georgiev@gmail.com>
2024-02-29 00:48:49 -0500

    ---refactored name-- Lab 01 "Jar of T" from the lesson for Generics, a part of Java Advanced course.

--- Generics/src/L01Jar/Main.java
+++ Generics/src/L01Jar/Main.java
@@ -2,7 +2,7 @@ package L01Jar;
 
 public class Main {
     public static void main(String[] args) {
-        Jar<Pickle> jar = new Jar<Pickle>();
+        Jar<Pickle> jar = new Jar<>();
 
         jar.add(new Pickle());
         jar.add(new Pickle());
